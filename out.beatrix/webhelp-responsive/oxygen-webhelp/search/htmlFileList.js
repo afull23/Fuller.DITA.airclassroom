5ec5e4f621ef848e2cbf55d0237df6ef27892359
afull23 <gandgfuller@Andrew-Fullers-MacBook-Pro.local>
2016-11-18 01:49:29 -0500

    Final Commit
    Includes all web help responsive by name

---
+++ out.beatrix/webhelp-responsive/oxygen-webhelp/search/htmlFileList.js
@@ -0,0 +1,15 @@
+//List of files which are indexed.
+fl = new Array();
+fl["0"]= "c-about-us.html";
+fl["1"]= "c-contact.html";
+fl["2"]= "c-refund.html";
+fl["3"]= "t-classroom-rules.html";
+fl["4"]= "t-document-camera.html";
+fl["5"]= "t-emergency.html";
+fl["6"]= "t-login.html";
+fl["7"]= "t-photoshop.html";
+fl["8"]= "t-projector.html";
+fl["9"]= "t-windows.html";
+fl["10"]= "tr-app-troubleshooting.html";
+fl["11"]= "tr-class-troubleshooting.html";
+var doStem = false;searchLoaded = true;
